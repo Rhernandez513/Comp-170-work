4725ff0785296f9637ef558718819263b87bc749
Rhernandez513 <rhernandez513@gmail.com>
2015-03-03 19:42:35 -0600

    Finished String Ops Lab

---
+++ StringEx_2.10.1/StringExcersize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntroCS
+{
+    class StringExcersize
+    {
+        static void Main()
+        {
+            string es;
+            es = "hola\n\n\nchico!";
+            Console.WriteLine(es);
+            es = @"hola
+
+
+chico!";
+            Console.WriteLine(es);
+        }
+    }
+}
---
+++ String_operations_lab/String_operations_Lab.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCS
+{
+    class StringOperationsLab
+    {
+        static void Main(string[] args)
+        {
+            TypeOfSentence();
+            for (int i = 0; i < 2; i++) // Loops twice so you don't have to run the program twice to see it works both ways
+            {
+                string userName = GetUserName();
+                NameReverse(userName);
+            }
+        }
+        //Gets initial string and decides on sentence type
+        public static void TypeOfSentence()
+        {
+            string type, s = UIF.PromptLine("Please enter a sentence ");
+            Console.WriteLine("The length of the string is " + s.Length + " characters.");
+            if (s.EndsWith("."))
+            {
+                type = ("declarative");
+            }
+            else if (s.EndsWith("!"))
+            {
+                type = ("exclamation");
+            }
+            else if (s.EndsWith("?"))
+            {
+                type = ("interrogatory");
+            }
+            else
+            {
+                type = ("not a sentence");
+            }
+            Console.WriteLine ("The sentence is " + type);
+        }
+        //Gets user name from input
+        public static string GetUserName()
+        {
+            string userInput = UIF.PromptLine("Please enter your first and last name on one line: ");
+            return userInput;
+        }
+        //performs name reversal if space exists, if not no change
+        public static void NameReverse(string userName)
+        {
+            int whitespace = userName.IndexOf(" ");
+            if (whitespace != -1)
+            {
+                string first = userName.Substring(0, (whitespace));
+                string last = userName.Substring((whitespace + 1));
+                string reversedName = (last + ", " + first);
+                userName = reversedName;
+                Console.WriteLine(userName);
+            }
+            else if (whitespace == -1)
+            {
+                Console.WriteLine(userName);
+            }
+        }
+    }
+}
