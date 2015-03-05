aa89e636c45751da40417a7a6521392b05174ad8
Rhernandez513 <rhernandez513@gmail.com>
2015-03-04 18:29:11 -0600

    Sum Exercise and housekeeping

---
+++ InteractiveSumExercise/sum_all.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCS
+{
+    class sum_all
+    {
+        static void Main(string[] args)
+        {
+            int userNumberInput = UI.PromptInt("Enter numbers to sum, one per" +
+                                              " line. Enter 0 to get sum: ");
+
+        }
+    }
+}
---
+++ LoopExcersize_6421/LoopExcersize_6421.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCS
+{
+    class LoopExcersize_6421
+    {
+        /// Checks if two consecutive characters in a string are the same
+        static void Main(string[] args)
+        {
+            string s = UI.PromptLine("enter string: ");
+            if (HasDoubleChar(s))
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Fail!");
+            }
+        }
+        static bool HasDoubleChar(string s)
+        {
+            int i = (s.Length - 1);
+            while (i < s.Length && i != 0)
+            {
+                if (s[i] == s[i-1])
+                {
+                    return true;
+                }
+                i--;
+            }
+            return false;
+        }
+    }
+}
