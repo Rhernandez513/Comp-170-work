fdc8c5b8ed0bce4db22cc3ef5bdd77cb1d1f4202
Rhernandez513 <rhernandez513@gmail.com>
2015-03-27 20:10:39 -0500

    Add object test file

---
+++ tester/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tester
+{
+   class GuessGameTest
+   {
+      public static void Main(){
+         List<string> a = new List<string> ();
+         a.Add ("pick up");
+         Item rope = new Item("rope",a,"hall");
+         Console.WriteLine (rope.getName ());
+         Console.WriteLine (rope.getLocation ());
+         foreach (string f in rope.getActions()) {
+            Console.WriteLine (f);
+         }
+         rope.setName ("new name");
+         Console.WriteLine (rope.getName ());
+        
+      }
+      public class Item{
+         private string name;
+         private List<string> actions;
+         private string location;
+         public Item(string Name, List<string> Actions,string Location){
+            name=Name;
+            actions=Actions;
+            location=Location;
+         }
+         public string getName (){
+            return name;
+         }
+         public List<string> getActions(){
+            return actions;
+         }
+         public string getLocation(){
+            return location;
+         }
+         public void setName (string name){
+            this.name = name;
+         }
+
+      }
+
+   }
+}
