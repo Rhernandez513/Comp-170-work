f2fbbb3c5ed6b672fbfd74d160e5087d56b7f559
Rhernandez513 <rhernandez513@gmail.com>
2015-03-29 23:44:28 -0500

    First New Object class, with program that creates object and prints object vars

--- tester/Class1.cs
+++ tester/Class1.cs
@@ -6,44 +6,74 @@ using System.Threading.Tasks;
 
 namespace tester
 {
-   class GuessGameTest
-   {
-      public static void Main(){
-         List<string> a = new List<string> ();
-         a.Add ("pick up");
-         Item rope = new Item("rope",a,"hall");
-         Console.WriteLine (rope.getName ());
-         Console.WriteLine (rope.getLocation ());
-         foreach (string f in rope.getActions()) {
-            Console.WriteLine (f);
-         }
-         rope.setName ("new name");
-         Console.WriteLine (rope.getName ());
-        
-      }
-      public class Item{
-         private string name;
-         private List<string> actions;
-         private string location;
-         public Item(string Name, List<string> Actions,string Location){
-            name=Name;
-            actions=Actions;
-            location=Location;
-         }
-         public string getName (){
-            return name;
-         }
-         public List<string> getActions(){
-            return actions;
-         }
-         public string getLocation(){
-            return location;
-         }
-         public void setName (string name){
-            this.name = name;
-         }
+  class Class1
+  {
+    //public static void Main(){
+    //   List<string> a = new List<string> ();
+    //   a.Add ("pick up");
+    //   Item rope = new Item("rope",a,"hall");
+    //   Console.WriteLine (rope.getName ());
+    //   Console.WriteLine (rope.getLocation ());
+    //   foreach (string f in rope.getActions()) {
+    //      Console.WriteLine (f);
+    //   }
+    //   rope.setName ("new name");
+    //   Console.WriteLine (rope.getName ());
 
+    //}
+    public class Item
+    {
+      private string name;
+      private List<string> actions;
+      private string location;
+      public Item(string Name, List<string> Actions, string Location)
+      {
+        name = Name;
+        actions = Actions;
+        location = Location;
       }
-
-   }
+      public string getName()
+      {
+        return name;
+      }
+      public List<string> getActions()
+      {
+        return actions;
+      }
+      public string getLocation()
+      {
+        return location;
+      }
+      public void setName(string name)
+      {
+        this.name = name;
+      }
+      public void setActions(List<string> actions)
+      {
+        this.actions = actions;
+      }
+      public void setLocation(string location)
+      {
+        this.location = location;
+      }
+      public void Print()
+      {
+        Console.Write(this.ToString());
+        ListToString(this.actions);
+      }
+      public override string ToString()
+      {
+        return string.Format("Item Name: {0}\nLocation: {1}\nProperties: ", 
+                              name, location);
+      }
+      private void ListToString(List<string> list)
+      {
+        foreach (string a in list)
+        {
+          Console.Write(a + ". ");
+        }
+        Console.WriteLine();
+      }
+    }
+  }
 }
---
+++ tester/Class3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tester
+{
+  public class Contact
+  {
+    private string name;
+    private string phone;
+    private string email;
+    public Contact(string name, string phone, string email)
+    {
+      this.name = name;
+      this.phone = phone;
+      this.email = email;
+    }
+    public string GetName()
+    {
+      return name;
+    }
+    public string GetPhone()
+    {
+      return phone;
+    }
+    public string GetEmail()
+    {
+      return email;
+    }
+    public void SetName(string newName)
+    {
+      this.name = newName;
+    }
+    public void SetPhone(string newPhone)
+    {
+      this.phone = newPhone;
+    }
+    public void SetEmail(string newEmail)
+    {
+      this.email = newEmail;
+    }
+    public void Print()
+    {
+      Console.WriteLine(ToString());
+    }
+    public override string ToString()
+    {
+      return string.Format("Name: {0}\nPhone: {1}\nEmail: {2}", name, phone, email);
+    }
+  }
+}
