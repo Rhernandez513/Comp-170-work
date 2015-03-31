60cf59fb8d5929df63f7ee6fa7dbaa6310d3c79e
Rhernandez513 <rhernandez513@gmail.com>
2015-03-31 08:20:19 -0500

    first version, buggy

--- Animal Class Lab/Class1.cs
+++ Animal Class Lab/Class1.cs
@@ -8,12 +8,36 @@ namespace IntroCS
 {
   class Animal
   {
-    private string name;
-    private List<string> gut;
+    public string name { get; set; }
+    public List<string> gut { get; set; }
     public Animal(string Name)
     {
       this.name = Name;
-      this.gut = null;
+      this.gut = new List<string>(new[]{"empty"});
+    }
+    public void Greet()
+    {
+      Console.WriteLine("Hello, my name is {0}", name);
+    }
+    public void Eat(string food)
+    {
+      gut.Add(food);
+    }
+    public void Excrete()
+    {
+      if (gut == null)
+      {
+        Console.WriteLine("");
+      }
+      else
+      {
+        Console.WriteLine(gut.First());
+        gut.Remove(gut.First());
+      }
+    }
+    public override string ToString()
+    {
+      return string.Format("Animal: {0}", name);
     }
   }
 }
--- Animal Class Lab/Program.cs
+++ Animal Class Lab/Program.cs
@@ -8,13 +8,22 @@ namespace IntroCS
 {
   class TestAnimal
   {
-     /// Thoroughly test all Animal methods, with all the actions 
-     /// Clearly labeled for a person *not* reading the code.
-     /// Start by creating a new Animal...
+    /// Thoroughly test all Animal methods, with all the actions 
+    /// Clearly labeled for a person *not* reading the code.
+    /// Start by creating a new Animal...
 
-static void Main(string[] args)
+    static void Main(string[] args)
     {
-      Animal Froggy = new Animal();
+      Animal Froggy = new Animal("Froggy");
+      Animal DonkeyKong = new Animal("Donkey Kong");
+
+      Froggy.Greet();
+      Froggy.Eat("fly");
+      Froggy.Eat("worm");
+      Froggy.Excrete();
+      Froggy.Excrete();
+      Froggy.Excrete();
+      Froggy.ToString();
       Console.WriteLine("TestAnimal still needs to be implemented");
     }
   }
