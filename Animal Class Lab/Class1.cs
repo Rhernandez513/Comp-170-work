b9d2b784d467d6245796d0b4611b476a5aae1980
Rhernandez513 <rhernandez513@gmail.com>
2015-03-30 21:41:14 -0500

    Animal Lab inital

---
+++ Animal Class Lab/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCS
+{
+  class Animal
+  {
+    private string name;
+    private List<string> gut;
+    public Animal(string Name)
+    {
+      this.name = Name;
+      this.gut = null;
+    }
+  }
+}
---
+++ Animal Class Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroCS
+{
+  class TestAnimal
+  {
+     /// Thoroughly test all Animal methods, with all the actions 
+     /// Clearly labeled for a person *not* reading the code.
+     /// Start by creating a new Animal...
+
+static void Main(string[] args)
+    {
+      Animal Froggy = new Animal();
+      Console.WriteLine("TestAnimal still needs to be implemented");
+    }
+  }
+}
