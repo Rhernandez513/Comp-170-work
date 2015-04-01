104703000f6cc886e2a1cd06d027276e14e82398
Rhernandez513 <rhernandez513@gmail.com>
2015-04-01 00:26:56 -0500

    -

--- Animal Class Lab/Class1.cs
+++ Animal Class Lab/Class1.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace IntroCS
 {
+  /// <summary>
+  /// An Animal can eat and excrete food
+  /// </summary>
   class Animal
-  {
+  { 
     public string name { get; set; }
     public List<string> gut { get; private set; }
     public int gutContents { get; private set; }
@@ -17,16 +20,26 @@ namespace IntroCS
       this.gut = new List<string>();
       this.gutContents = gutContents;
     }
+    /// <summary>
+    /// Animal says hi
+    /// </summary>
     public void Greet()
     {
       Console.WriteLine("Hello, my name is {0}", name);
     }
+    /// <summary>
+    /// Adds food to gut
+    /// </summary>
+    /// <param name="food">food Animal eats as string</param>
     public void Eat(string food)
     {
       Console.WriteLine("{0} is eating {1}", name, food);
       gut.Add(food);
       this.gutContents++;
     }
+    /// <summary>
+    /// Removes contents of gut in order they were put in
+    /// </summary>
     public void Excrete()
     {
       if (gut.Count == 0)
@@ -40,6 +53,10 @@ namespace IntroCS
         this.gutContents--;
       }
     }
+    /// <summary>
+    /// Checks what the Animal has in its gut
+    /// </summary>
+    /// <returns>String saying what is currently in the Animal's gut</returns>
     public override string ToString()
     {
       if (gut.Count == 0)
@@ -52,15 +69,15 @@ namespace IntroCS
       }
       StringBuilder gutStr = new StringBuilder();
       gutStr = gutStr.AppendFormat("Animal: {0} ate ", name);
-      foreach (string s in gut)
+      for (int i = 0; i < gutContents; i++ )
       {
-        if (s == gut.Last())
+        if (gut[i] == gut.Last())
         {
-          gutStr = gutStr.AppendFormat("and {0}", s);
+          gutStr = gutStr.AppendFormat("and {0}", gut[i]);
         }
         else
         {
-          gutStr = gutStr.AppendFormat("{0}, ", s);
+          gutStr = gutStr.AppendFormat("{0}, ", gut[i]);
         }
       }
       return gutStr.ToString();
--- Animal Class Lab/Program.cs
+++ Animal Class Lab/Program.cs
@@ -8,10 +8,6 @@ namespace IntroCS
 {
   class TestAnimal
   {
-    /// Thoroughly test all Animal methods, with all the actions 
-    /// Clearly labeled for a person *not* reading the code.
-    /// Start by creating a new Animal...
-
     static void Main(string[] args)
     {
       Animal Froggy = new Animal("Froggy");
