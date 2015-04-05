018bdc5cab953a395c0e90bff6f6cf8086f1de40
Rhernandez513 <rhernandez513@gmail.com>
2015-04-05 13:51:23 -0500

    Read cmd line args, if none, promptline

--- Hmwk3/hmwk3.cs
+++ Hmwk3/hmwk3.cs
@@ -10,6 +10,29 @@ namespace IntroCS
   {
     static void Main(string[] args)
     {
+      string CourseAbbreviation = GetAbbrev(args);
+    }
+    /// <summary>
+    /// Gets command line args and returns as a string
+    /// </summary>
+    /// <param name="args">CMD Line Args</param>
+    /// <returns></returns>
+    private static string GetAbbrev(string[] args)
+    {
+      if (args.Any<string>())
+      {
+        StringBuilder Abbrev = new StringBuilder();
+        foreach (string s in args)
+        {
+          Abbrev.Append(s);
+        }
+        return "" + Abbrev;
+      }
+      else
+      {
+        string Abbrev = UI.PromptLine("Please enter a course Abbreviation: ");
+        return Abbrev;
+      }
     }
   }
 }
